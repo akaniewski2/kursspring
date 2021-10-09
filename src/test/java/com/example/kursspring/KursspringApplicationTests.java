de64af5c062bd9625feee84c37ed618f003ab99f
akaniewski <47106446+akaniewski@users.noreply.github.com>
2021-10-09 23:35:46 +0200

    Initial commit

---
+++ src/test/java/com/example/kursspring/KursspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
