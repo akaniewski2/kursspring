59338cf8413000f44858622e984e05fda532dfea
akaniewski <47106446+akaniewski@users.noreply.github.com>
2021-10-10 22:17:40 +0200

    L: 7 Rodzaje wstrzykiwania zależności w Spring Framework + @componentscan

---
+++ src/main/java/com/example/component/TestComponent.java
@@ -0,0 +1,21 @@
+package com.example.component;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class TestComponent {
+
+    @PostConstruct
+    public void testVoid(){
+        System.out.println("Zaladowano TestComponent");
+    }
+
+    @PreDestroy
+    public void testVoid2(){
+        System.out.println("Zniszczono TestComponent");
+    }
+
+}
--- src/main/java/com/example/kursspring/KursspringApplication.java
+++ src/main/java/com/example/kursspring/KursspringApplication.java
@@ -2,7 +2,10 @@ package com.example.kursspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan({"com.example.kursspring","com.example.component"})
 @SpringBootApplication
 public class KursspringApplication {
 
