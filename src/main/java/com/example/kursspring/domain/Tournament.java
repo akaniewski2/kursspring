517391a5d5d54d27917efc7593f75c7b8fce0b63
akaniewski <47106446+akaniewski@users.noreply.github.com>
2021-10-16 19:29:12 +0200

    L 4.4 Wstrzykiwanie kolekcji

--- src/main/java/com/example/kursspring/domain/Tournament.java
+++ src/main/java/com/example/kursspring/domain/Tournament.java
@@ -4,19 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+
 @Component
 public class Tournament {
 
     @Autowired
-    @Qualifier(value="percival")
-    Knight knight;
+   // @Qualifier(value="percival")
+//    knights knights;
+//    List<Knight> knights;
+
+    Map<String, Knight> knights; // map - mozna ogranczac wstrzykiwane beany, map wstrzykujemy tylko przy pomocy xml
+
+//            Set<Knight> knights;
 
 //    @Autowired
-//    public Tournament( @Qualifier(value="percival") Knight knight) {
-//        this.knight = knight;
+//    public Tournament( @Qualifier(value="percival") knightss knights) {
+//        this.knights = knights;
 //    }
 
 
+
+
+//   public void setKnights(List<Knight> knights){this.knights=knights;}
+   // public void setKnights(Set<Knight> knights){this.knights=knights;}
+public void setKnights(Map<String,Knight> knights){this.knights=knights;}
 
 
 
@@ -28,10 +45,11 @@ public class Tournament {
     }
 
     public void duel (){
-        knight.setAge(knight.getAge()+1);
+       // knights.setAge(knights.getAge()+1);
     }
     @Override
     public String toString() {
-        return "W turnieju bierze udzial rycerz "+knight ;
+    // przy map sie to krzaczy  - trzeba by przerobic
+        return "W turnieju bierze udzial rycerz(e) "+ knights.stream().map(Object::toString).collect(Collectors.joining(",")) ;
     }
 }
